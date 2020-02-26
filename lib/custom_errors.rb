43b8b77f82168c75f9553e20626202ef3ac85327
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-26 22:07:17 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,28 +1,31 @@
-class Person
-attr_accessor :name, :partner
+require 'pry'
+class Triangle
 
-  def initialize(name)
-    @name = name
+  def initialize(a,b,c)
+  @array = [a,b,c].sort
   end
-  class ErrorPartner < StandardError
+
+  def isc
+    @array[1] == @array[0]|| @array[2]==@array[1] ? true : false
+  end
+  def eq
+    @array[1] == @array[0] && @array[1]==@array[2]
+  end
+  def sc
+    @array[0]>@array[1]+@array[2]
+  end
+  def triangleType
+    eq ? "equilateral" : isc ? "iscoseles" : sc ? "scalene": undef
+  end
+  
+  class TriangleError < StandardError
     def message
-       "fuckity fuckity fuckernutz"
-    end
-  end
-  def getMarried(person)
-    if person.class != Person
-        begin
-          raise ErrorPartner
-        rescue ErrorPartner => error
-          puts error.message
-        end
-    else
-      person.partner = self
+      "fuck shit shit fuck, ball: sac, nuts, chees"
     end
   end
 end
 
-
-beyonce = Person.new("Beyonce")
-beyonce.getMarried("Jay-Z")
-puts beyonce.name
+a = Triangle.new(1,1,1)
+b = Triangle.new(1,2,2)
+c = Triangle.new(1,2,99999)
+binding.pry
